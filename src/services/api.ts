ee2e84ee61cd4de6a85bb0a14e3923815281227a
Thiago <thiago_murakami@hotmail.com>
2021-04-29 14:04:11 -0300

    clean and add axios

---
+++ src/services/api.ts
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+// Axios, cliente HTTP basedo em Promise para fazer requisicoes
+// tmb 'e possivel reproduzir com o fetch()
+const api = axios.create({
+    baseURL: "https://pokeapi.co/api/v2/",
+});
+
+export default api;
