5c7943e7b4392e3a05f6b2eac7bee52c5487c5db
Thiago <thiago_murakami@hotmail.com>
2021-05-03 14:52:00 -0300

    show the pokemon in label searchbar

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,57 +1,65 @@
 import React, { useState } from 'react';
-import {Form, Col, Button, Card} from 'react-bootstrap';
+import {Form, Col, Button, Container} from 'react-bootstrap';
 
 import api from '../../services/api';
 
 export default function Search(props) {
 
     // retorna um valor e uma funcao para atualizar o valor
-    const [pokemon, setPokemon] = useState("pikachu");
-    const [pokemonData, setPokemonData] = useState([]);
+    const [pokemonName, setPokemonName] = useState("")
+    const [pokemon, setPokemon] = useState({
+        name: "",
+        img: "",
+    });
 
     // retorna o valor convetido para minusculo
     const handleChange = (e) => {
-        setPokemon(e.target.value.toLowerCase());
+        setPokemonName(e.target.value.toLowerCase());
     };
     
     // cancela o evento, sem para a programacao 
     const handleSubmit = (e) => {
         e.preventDefault();
-        getPokemon();
+        getSelectPokemon();
     };
     
-    // realiza um busca na api
-    const getPokemon = async () => {
-        const toArray = [];
-        try {
-          const resp = await api.get(`/pokemon/${pokemon}`);
-          toArray.push(resp.data);
-          setPokemonData(toArray);
-          console.log(resp)
-        } catch (e) {
-          console.log(e);
-        }
-        console.log(pokemonData);
+    // realiza a busca no api retornando o pokemon
+    const getSelectPokemon = async () => {
+        
+        api.get(`pokemon/${pokemonName}`)
+        .then ((response) => {
+            setPokemon({
+                name: response.data.name,
+                img: response.data.sprites.front_default
+            })
+            console.log(response);
+        })
+        .catch((err) => {
+            console.error("Pokemon n encontrado" + err)
+        })
     };
     
     return (
-        <Form.Row className="align-items-center">
-            <Col sm={6} className="my-1">
-                <Form.Label htmlFor="inlineFormInputName" srOnly>
-                Pokemon
+        <Container>
+            <Form.Row className="align-items-center">
+                <Col sm={6} className="my-1">
+                    <Form.Label htmlFor="inlineFormInputName" srOnly>
                 </Form.Label>
                 <Form.Control 
                     id="inlineFormInputName"
                     onChange={handleChange}
                     placeholder="Pokemon" />
-            </Col>
-            <Col xs="auto" className="my-1">
-                <Button 
-                    type="submit" 
-                    onClick={handleSubmit}>
-                Procurar
-                </Button>
-            </Col>
-        </Form.Row>        
+                </Col>
+                <Col xs="auto" className="my-1">
+                    <Button block
+                        type="submit"
+                        onClick={handleSubmit}>
+                    Procurar
+                    </Button>
+                </Col>
+            </Form.Row>
+            <h1>{pokemon.name}</h1>
+            <img src={pokemon.img}/>
+        </Container>
     )
 }
